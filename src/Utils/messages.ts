aed2783518dcf17ae326a19a9283e1030e73eef5
Zakar Rafalph <101258561+zakinshk@users.noreply.github.com>
2024-08-17 00:27:51 -0300

    Update list type 2 messages.ts

--- src/Utils/messages.ts
+++ src/Utils/messages.ts
@@ -571,7 +571,7 @@ export const generateWAMessageContent = async(
 			title: message.title,
 			footerText: message.footer,
 			description: message.text,
-			listType: proto.Message.ListMessage.ListType.SINGLE_SELECT
+			listType: proto.Message.ListMessage.ListType.SINGLE_SELECT,
 			listType: message.hasOwnProperty("listType") ? message.listType : proto.Message.ListMessage.ListType.PRODUCT_LIST
 		}
 
